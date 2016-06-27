5bf64ba4fda1781c8a3ec1e845c541cbc8ef528d
Alex <maiorrulz@gmail.com>
2016-06-28 00:28:34 +0300

    Fixes of UI Tests

--- app/src/androidTest/java/com/ui/test/BusinessTest.java
+++ app/src/androidTest/java/com/ui/test/BusinessTest.java
@@ -1,51 +1,105 @@
 package com.ui.test;
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.view.View;
 
+import org.hamcrest.Matcher;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import allgedera.com.allgederaapp.R;
+import allgedera.com.allgederaapp.menus.MainActivity;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 //============================================================================================
 //============================================================================================
-
 /**
  * Created by Alex on 4/23/2016.
  */
 @RunWith(AndroidJUnit4.class)
 public class BusinessTest {
 
-    //@Rule
-    //public ActivityTestRule<MainMenuActivity> mActivityRule =new ActivityTestRule<>(MainMenuActivity.class);
+    private UiDevice mDevice;
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void getToBusinesses() {
-        onView(withId(R.id. btn_buisnesses)).perform(click());
+    public void businessTest() {
 
-        onView(withId(R.id.pagerTabStrip)).check(matches(isDisplayed()));
+        onView(withId(R.id. btn_fbLogin)).perform(click());
+        try {
+            Thread.sleep(5000);
+            onView(allOf(withId(R.id.btn_buisnesses), isDisplayed())).perform(click());
+            Thread.sleep(5000);
+            //onView(allOf(withId(R.id.businessRecyclerView))).perform(click());
+            Matcher<View> smallTalkView = hasSibling(withText("Small Talk"));
+            onView(allOf(withId(R.id.tv_businessName), smallTalkView)).perform(click());
+            Thread.sleep(1500);
+            onView(allOf(withId(R.id.tv_businessName), smallTalkView)).perform(click());
+            Thread.sleep(1500);
+            Matcher<View> pixelView = hasSibling(withText("פיקסל אלבומים גידיטליים"));
+            onView(allOf(withId(R.id.tv_businessName), pixelView)).perform(click());
+            Thread.sleep(1500);
+            onView(allOf(withId(R.id.tv_businessName), pixelView)).perform(click());
+            Thread.sleep(1500);
+            pressBack();
+            Thread.sleep(1500);
+            onView(withId(R.id.btn_buisnesses)).check(matches(isDisplayed()));
+
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //onWebView().withElement(findElement(Locator.ID, "Email or Phone")).perform(DriverAtoms.webKeys("ssss"));
+//        try {
+//        Thread.sleep(5000);
+//        UiObject input = mDevice.findObject(new UiSelector()
+//                .instance(1)
+//                .className(EditText.class));
+//
+//            input.setText("text");
+//            Thread.sleep(5000);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
 
     }
 
     @Test
-    public void getToSpecificBusiness() {
-        onView(withId(R.id.btn_buisnesses)).perform(click());
-        onView(allOf(withId(R.id.businessRecyclerView), isDisplayed())).perform(click());
+    public void mapToBusinessTest() {
+        onView(withId(R.id.btn_fbLogin)).perform(click());
         try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        onView(withId(R.id.businessRecyclerView)).check(matches(isDisplayed()));
+            Thread.sleep(5000);
+            onView(allOf(withId(R.id.btn_buisnesses), isDisplayed())).perform(click());
+            Thread.sleep(3000);
+            onView((withId(R.id.pagerTabStrip))).perform(swipeLeft());
+            onView(withId(R.id.businesses_map)).check(matches(isDisplayed()));
+            onView((withId(R.id.pagerTabStrip))).perform(swipeRight());
+            pressBack();
+            Thread.sleep(3000);
+            onView(withId(R.id.btn_buisnesses)).check(matches(isDisplayed()));
+            //onView(allOf(withId(R.id.pagerTabStrip), withText("מפת עסקים"))).perform(click());
 
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
     }
 
 
@@ -59,4 +113,4 @@ public class BusinessTest {
 //            // This view is in a different Activity, no need to tell Espresso.
 //            onView(withId(R.id.resultView)).check(matches(withText("NewText")));
 //        }
-    }
+}
